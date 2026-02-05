a58d648b9ad2de43451449bf064b542407ec32c0
fubaydullaev <fubaydullaev@gmail.com>
2026-02-05 14:16:03 +0500

    fix: switch to setuptools and remove init hack

--- nasiya365/__init__.py
+++ nasiya365/__init__.py
@@ -5,7 +5,3 @@ Buy Now, Pay Later with Inventory & Sales Management
 
 __version__ = "0.0.1"
 
-import sys
-# Hack to support legacy/redundant module path required by Frappe
-# when app_name == module_name
-sys.modules["nasiya365.nasiya365"] = sys.modules["nasiya365"]
